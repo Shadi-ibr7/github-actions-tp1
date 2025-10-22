3d5927db14fe08599e2b677046931dedb119878e
Chadi Ibrahim <ishadi799@gmail.com>
2025-10-22 11:58:24 +0200

    introduce bug

--- model.py
+++ model.py
@@ -6,3 +6,5 @@ def predict_sentiment(text):
     if "sad" in text.lower() or "bad" in text.lower():
         return "negative"
     return "neutral"
+    if "happy" in text.lower() or "good" in text.lower():
+        return "positive"  # Bug volontaire
